19f3a2ab04872136f32ddcc43d8b2452c6cb265e
sipalingoding <arsenalsalmanfpl@gmail.com>
2025-10-16 16:07:16 +0700

    fixing params

--- app/api/dongeng/[id]/reject/route.ts
+++ app/api/dongeng/[id]/reject/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export async function PATCH(req: Request, context: { params: { id: string } }) {
+export async function PATCH(req: Request, context: any) {
   const { id } = context.params;
   const cookieStore = cookies();
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
